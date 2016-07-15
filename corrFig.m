7588d6f1c65e95996cc1706ea30560bc5c545043
Aster Vanhecke <aster.vanhecke@gmail.com>
2016-07-15 17:50:12 +0200

    Added linear fit formula to figure

--- corrFig.m
+++ corrFig.m
@@ -2,6 +2,10 @@ function [corCof]=corrFig(X,Y)
 % Function to examine correlation between vector X and Y.
 % Plot scatterplot, draw a line, calculate and display some correlation
 % coefficients. Maybe more.
+% Getting input variable names doesnt work for expressions, e.g. Lg-Lc.
+% As a temporary workaround you could define them as a single variable,
+% e.g.: ElongAfterConstr=Lg-Lc; and use this as input for corrFig.
+
 figure,
 hold on
 X=reshape(X, [], 1);
@@ -10,6 +14,7 @@ cfit=fit(X,Y,'poly1');
 % plot(X,Y,'.')
 plot(cfit,X,Y,'predfunc')
 title(['Correlation test between ' inputname(1) ' and ' inputname(2)])
+legend('Data', ['Fitted curve: Y= ' num2str(cfit.p1) '*X + ' num2str(cfit.p2)], 'prediction bounds')
 corCof=corr(X,Y);
 xlabel(inputname(1))
 ylabel(inputname(2))
