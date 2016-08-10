706440696893558a211303529d35f33e36099dff
Aster Vanhecke <aster.vanhecke@gmail.com>
2016-08-10 18:09:26 +0200

    corrFig title
    to corrFig, added the option to input a figure title

--- corrFig.m
+++ corrFig.m
@@ -1,4 +1,4 @@
-function [corCof]=corrFig(X,Y)
+function [corCof]=corrFig(varargin)
 % Function to examine correlation between vector X and Y.
 % Plot scatterplot, draw a line, calculate and display some correlation
 % coefficients. Maybe more.
@@ -6,6 +6,12 @@ function [corCof]=corrFig(X,Y)
 % As a temporary workaround you could define them as a single variable,
 % e.g.: ElongAfterConstr=Lg-Lc; and use this as input for corrFig.
 
+narginchk(2,3);
+X=varargin{1};
+Y=varargin{2};
+if size(varargin,2)==3
+    figTitle=varargin{3};
+end
 figure,
 hold on
 X=reshape(X, [], 1);
@@ -13,11 +19,14 @@ Y=reshape(Y, [], 1);
 cfit=fit(X,Y,'poly1');
 % plot(X,Y,'.')
 plot(cfit,X,Y,'predfunc')
-title(['Correlation test between ' inputname(1) ' and ' inputname(2)])
 legend('Data', ['Fitted curve: Y= ' num2str(cfit.p1) '*X + ' num2str(cfit.p2)], 'prediction bounds')
 corCof=corr(X,Y);
 xlabel(inputname(1))
 ylabel(inputname(2))
-title(['Correlation test between ' inputname(1) ' and ' inputname(2) ', correlation coefficient ' num2str(corCof)])
+if size(varargin,2)==3
+    title([figTitle ', correlation coefficient ' num2str(corCof)])
+else
+    title(['Correlation test between ' inputname(1) ' and ' inputname(2) ', correlation coefficient ' num2str(corCof)])
+end
 
 end
