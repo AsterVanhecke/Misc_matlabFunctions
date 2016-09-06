a82a7dcf24d3d8a0f735412df5ac7d844f9e25cb
Aster Vanhecke <aster.vanhecke@gmail.com>
2016-09-06 11:10:30 +0200

    plot Tc's on kymographs
    new inputs: T0, to correct starting time on kymographs
    Tc: matrix with Tc's (or other times of interest) to plot on kymographs
    involving some input detection tricks

--- kymograph.m
+++ kymograph.m
@@ -1,6 +1,6 @@
-function M=kymograph(ZintAll,demo)
+function M=kymograph(ZintAll,demogr,varargin)
 % function to plot kymograph
-% kymograph(ZintAll,demo)
+% M=kymograph(ZintAll,demogr,varargin)
 % inputs:
 %--------
 % ZintAll: cell(matlab) array, dimenions: cells(bacteria)-by-frames. Each
@@ -9,10 +9,46 @@ function M=kymograph(ZintAll,demo)
 % coordinates. (for now only supports equally spaced lengths with the same
 % interval for each cell). The second row contains the intensity value for
 % the respective lengths.
-% demo: picks which type of kymograph to plot: 1 for kymograph per cell, 2
+% demogr: picks which type of kymograph to plot: 1 for kymograph per cell, 2
 % for overall demograph (ordered by length, discards time info), 3:
-% demograph per timepoint
+% length-ordered demograph per timepoint
+% optional input:
+% Tc: matrix of different times of interest for each cell(e.g. measured constriction
+% onset) to be plotted on the kymographs (option demogr==1).
+% [numberOfCells N]=size(Tc), with N the number of times you want to plot
+% for each cell.
+% T0: Integer. Time value to assign to first timepoint. either specify
+% as 'T0' or 't0', or just insert a single value.
+% Output: M: matrix for imageplot of last plot, most useful for the
+% demograph.
 
+narginchk(2,6)
+inputi=1;
+
+if demogr==1 % get optional input if needed
+    while inputi <= length(varargin)
+        if size(varargin{1,inputi},1)==size(ZintAll,1)
+            Tc=varargin{1,inputi};
+            inputi=inputi+1;
+        elseif length(varargin{1,inputi})==2
+            switch varargin{1,inputi}
+                case {'tc','Tc'}
+                    Tc=varargin{1,inputi+1};
+                    inputi=inputi+2;
+                case {'t0','T0'}
+                    T0=varargin{1,inputi+1};
+                    inputi=inputi+2;
+                otherwise
+                    error(['Input argument ' num2str(inputi+2) ' is invalid!'])
+            end
+        elseif isnumeric(varargin{1,inputi}) && length(varargin{1,inputi})==1
+            T0=varargin{1,inputi};
+            inputi=inputi+1;
+        else
+            error(['Input argument ' num2str(inputi+2) ' is invalid!'])
+        end
+    end
+end
 
 % find the overal longest cell.
 lengthz=cellfun(@length,ZintAll);
@@ -20,7 +56,9 @@ maxLength=max(max(lengthz));
 maxLength=2*ceil((maxLength+1)/2); % increase value of maxLength to next even integer to prevent bugs
 [num_cells, num_frames]=size(ZintAll);
 
-switch demo
+warning('off','MATLAB:legend:IgnoringExtraEntries') % supress legend warning
+
+switch demogr
     case 1
         % Kymographs for each cell
         for cellIdx=1:num_cells
@@ -33,9 +71,18 @@ switch demo
                         ZintAll{cellIdx,frIdx}(2,:)';
                 end
             end
-            x=1:maxFrame;x=x.*5;
+            x=0:(maxFrame-1);x=x.*5;
+            if exist('T0','var')
+                x=x+T0;
+            end
             y=-maxLength*30:60:maxLength*30;
             plotKymo(M,x,y)
+            if exist('Tc','var')
+                for ii=1:size(Tc,2)
+                    plot([Tc(cellIdx,ii) Tc(cellIdx,ii)], [y(1) y(end)])
+                    legend('midcell','TcF','TcX','Tz')
+                end
+            end
         end
     case 2
         % Demograph for all cells
